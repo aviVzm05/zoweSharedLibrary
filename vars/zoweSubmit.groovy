21f3223fc6816ef66cb70c08e238e9d40c66d91f
avinash <avi.vzm05@gmail.com>
2022-03-07 15:49:26 +0530

    correction to submit command.. executed successfully

--- vars/zoweSubmit.groovy
+++ vars/zoweSubmit.groovy
@@ -4,13 +4,8 @@
 **/
 
 def  call(Map config) {
-    echo "${config.jobname}"
-    echo "${config.successfulRC}"
-
     RETURN_CODE = sh (returnStdout: true,
                       script: "zowe jobs submit ds \'${config.jobname}\' --wfo --rft string --rff retcode").trim().split()
-    
-    echo "${RETURN_CODE[1]}"
 
     if (!(config.successfulRC.contains(RETURN_CODE[1]))) {
         error ("${config.jobname} failed with return code ${RETURN_CODE[1]}")
